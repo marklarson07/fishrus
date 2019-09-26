8981cc294c668f8d4c98851078bad28dbd07d8bd
marklarson07 <54913917+marklarson07@users.noreply.github.com>
2019-09-25 18:34:32 -0600

    Add files via upload

---
+++ FishRUs/distributorlogin.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Distributor Login</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="styles.css" >
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+</head>
+<body>
+<!--- navbar --->
+
+<nav class="navbar navbar-expand-md bg-dark navbar-dark">
+    <!-- Brand -->
+    <a class="navbar-brand" href="index.php">Fish R US</a>
+
+    <!-- Toggler/collapsibe Button -->
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <!-- Navbar links -->
+    <div class="collapse navbar-collapse" id="collapsibleNavbar">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="aboutus.php">About Us</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="contactus.php">Contact Us</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="shopfish.php">Shop Fish</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="shopsupplies.php">Shop Supplies</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="customerlogin.php">Customer Login</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="distributorlogin.php">Distributor Login</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="cart.php">Cart</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+
+<!--- Content --->
+<div class="jumbotron jumbotron-fluid text-center">
+    <div class="container">
+        <h1 align="center">Distributor Login</h1>
+    </div>
+</div>
+<br><br><br><br>
+<div class="login-form">
+    <div class="row">
+        <div class="col-sm-4"></div>
+        <div class="col-sm-4">
+            <div class="mx-auto">
+                <form action="#" method="post">
+                    <div class="form-group">
+                        <input type="text" class="form-control" placeholder="Username" required="required">
+                    </div>
+                    <div class="form-group">
+                        <input type="password" class="form-control" placeholder="Password" required="required">
+                    </div>
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-secondary btn-block">Log in</button>
+                    </div>
+                    <div class="clearfix">
+                        <label class="pull-left checkbox-inline"><input type="checkbox"> Remember me</label>
+                        <a href="#" class="pull-right">Forgot Password?</a>
+                    </div>
+                </form>
+                <p class="text-center"><a href="#">Create an Account</a></p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!--- Scripting --->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+</body>
+</html>
+
+
---
+++ FishRUs/index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Home Page</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="styles.css" >
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+</head>
+<body>
+<!--- navbar --->
+
+<nav class="navbar navbar-expand-md bg-dark navbar-dark">
+    <!-- Brand -->
+    <a class="navbar-brand" href="index.php">Fish R US</a>
+
+    <!-- Toggler/collapsibe Button -->
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <!-- Navbar links -->
+    <div class="collapse navbar-collapse" id="collapsibleNavbar">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" href="aboutus.php">About Us</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="contactus.php">Contact Us</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="shopfish.php">Shop Fish</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="shopsupplies.php">Shop Supplies</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="customerlogin.php">Customer Login</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="distributorlogin.php">Distributor Login</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="cart.php">Cart</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+
+<!--- Content enter body here --->
+<div class="jumbotron jumbotron-fluid text-center">
+    <div class="container">
+        <h1>Home Page</h1>
+    </div>
+</div>
+<div class="container">
+    <p>LLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Viverra nibh cras pulvinar mattis nunc sed blandit. Placerat orci nulla pellentesque dignissim. Egestas congue quisque egestas diam in arcu cursus euismod quis. Sodales ut etiam sit amet nisl purus. Nam libero justo laoreet sit amet cursus sit. Eleifend mi in nulla posuere. Gravida rutrum quisque non tellus orci ac. Laoreet suspendisse interdum consectetur libero id faucibus nisl tincidunt. Pellentesque habitant morbi tristique senectus et. Nulla pellentesque dignissim enim sit amet. Urna nec tincidunt praesent semper feugiat nibh sed. Tellus mauris a diam maecenas sed enim ut sem. Turpis nunc eget lorem dolor sed viverra ipsum. Egestas fringilla phasellus faucibus scelerisque eleifend donec. Gravida dictum fusce ut placerat orci. Adipiscing bibendum est ultricies integer quis auctor elit. Mauris sit amet massa vitae tortor condimentum.<br><br>
+
+        Commodo nulla facilisi nullam vehicula ipsum. Ac tincidunt vitae semper quis lectus nulla at volutpat diam. Pulvinar elementum integer enim neque volutpat ac tincidunt. Posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Ut diam quam nulla porttitor massa id neque aliquam. Adipiscing elit duis tristique sollicitudin. Vivamus at augue eget arcu dictum varius duis. Scelerisque mauris pellentesque pulvinar pellentesque habitant morbi. Tempor nec feugiat nisl pretium fusce id velit ut. Felis eget velit aliquet sagittis id consectetur purus ut. Mauris pharetra et ultrices neque. Commodo quis imperdiet massa tincidunt nunc. Commodo odio aenean sed adipiscing diam donec adipiscing tristique. Aliquet sagittis id consectetur purus. Vestibulum lectus mauris ultrices eros in cursus turpis. Morbi tristique senectus et netus et malesuada fames ac. Sit amet risus nullam eget. Massa sapien faucibus et molestie ac feugiat. In nisl nisi scelerisque eu. Laoreet non curabitur gravida arcu ac tortor.</p>
+</div>
+
+
+<!--- Scripting --->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+</body>
+</html>
+
+
